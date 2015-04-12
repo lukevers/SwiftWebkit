31aebf95cff4afe555b5d919b74c2a310b47d269
Luke Evers <me@lukevers.com>
2015-04-12 15:41:21 -0400

    Changes

--- SwiftWebkit/ViewController.swift
+++ SwiftWebkit/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var containerView : UIView! = nil
+    var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.webView = WKWebView()
+        self.view = self.webView!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var url = NSURL(string:"http://lukevers.com/")
+        var req = NSURLRequest(URL:url!)
+        self.webView!.loadRequest(req)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
-
